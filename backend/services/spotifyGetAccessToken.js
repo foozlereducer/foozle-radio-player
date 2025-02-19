bab23a0ab9e5081bff0b9141b60f7fb12b8dec55
unknown <foozlereducers@gmail.com>
2025-02-19 11:29:42 -0500

    resolved the Spotify access token error.

--- backend/services/spotifyGetAccessToken.js
+++ backend/services/spotifyGetAccessToken.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
+import dotenv from 'dotenv'
+dotenv.config();
+
 
 let spotifyAccessToken = null;
-const SPOTIFY_CLIENT_ID = import.meta.SPOTIFY_CLIENT_ID;
-const SPOTIFY_CLIENT_SECRET = import.meta.SPOTIFY_CLIENT_SECRET;
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID?.trim();
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET?.trim();
+
 
 /**
  * Fetch Spotify access token using Client Credentials flow.
  */
 export const fetchSpotifyAccessToken = async () => {
+
   if (spotifyAccessToken) {
     return spotifyAccessToken; // Use cached token
   }
@@ -20,6 +25,7 @@ export const fetchSpotifyAccessToken = async () => {
         'Content-Type': 'application/x-www-form-urlencoded',
         Authorization: `Basic ${Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64')}`,
       },
+     
     });
 
     spotifyAccessToken = response.data.access_token;
